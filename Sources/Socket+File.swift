7eee4b024c38e712560f78c376affb52d3d59131
Tomasz Pawlowski <tptpawlowski@gmail.com>
2020-09-30 16:41:24 +0200

    udpated deprecations

--- Sources/Socket+File.swift
+++ Sources/Socket+File.swift
@@ -11,16 +11,19 @@ import Foundation
     struct sf_hdtr { }
     
     private func sendfileImpl(_ source: UnsafeMutablePointer<FILE>, _ target: Int32, _: off_t, _: UnsafeMutablePointer<off_t>, _: UnsafeMutablePointer<sf_hdtr>, _: Int32) -> Int32 {
-        var buffer = [UInt8](repeating: 0, count: 1024)
+        let BUFFER_SIZE = 1024
+        let buffer = UnsafeMutableRawPointer.allocate(byteCount: BUFFER_SIZE, alignment: 1)
         while true {
-            let readResult = fread(&buffer, 1, buffer.count, source)
+            let readResult = fread(buffer, 1, BUFFER_SIZE, source)
             guard readResult > 0 else {
+                buffer.deallocate()
                 return Int32(readResult)
             }
             var writeCounter = 0
             while writeCounter < readResult {
-                let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)
+                let writeResult = write(target, buffer + writeCounter, readResult - writeCounter)
                 guard writeResult > 0 else {
+                    buffer.deallocate()
                     return Int32(writeResult)
                 }
                 writeCounter = writeCounter + writeResult
